72a03e58514d6bf1ec336a006caa70b28cc71d70
Nindya Athifa <168733133+nindyathf@users.noreply.github.com>
2026-04-08 11:39:27 +0700

    submit exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    if (b<0) {
+        return 'ERROR';
+    }
+    let teksHasil = "";
+    for (let i=0; i<b; i++) {
+        teksHasil+= a;
+    }
+    return teksHasil;
 };
 
 // Do not edit below this line
