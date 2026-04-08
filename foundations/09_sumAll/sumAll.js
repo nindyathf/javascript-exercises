a739aa9638729bdaf2654ea775d2db257f014fdc
Nindya Athifa <168733133+nindyathf@users.noreply.github.com>
2026-04-08 14:38:26 +0700

    submit exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    min = Math.min(a, b)
+    max = Math.max(a, b)
+    let total = 0;
+    for (i=min; i<= max; i++) {
+        total += i
+    }
+    return total;
 };
 
 // Do not edit below this line
