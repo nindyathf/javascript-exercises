726b2613c3842caaa78263a81c7f73a0db9588eb
Nindya Athifa <168733133+nindyathf@users.noreply.github.com>
2026-04-08 15:16:01 +0700

    submit exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(suhu) {
+  return suhuCelcius = Math.round((suhu - 32) * (5/9))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(suhu) {
+  return suhuFahrenheit = Math.round(suhu * (9/5) + 32)
 };
 
 // Do not edit below this line
