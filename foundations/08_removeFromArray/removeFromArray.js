9b9679c1171e27e8fecce58c2104df977b65ecfe
Nindya Athifa <168733133+nindyathf@users.noreply.github.com>
2026-04-08 13:55:32 +0700

    submit exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemRemove) {
+    return arr.filter(item => {
+        itemRemove.includes(item)
+        return !itemRemove.includes(item)
+    });
 };
 
 // Do not edit below this line
